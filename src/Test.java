81e013ef0b5a9f16f86e964e107e3f9bcb3284f2
andrearizzieri <andrearizzieri@imac-di-andrea.homenet.telecomitalia.it>
2020-03-10 11:56:54 +0100

    Aggiunta seconda stampa su test

--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,10 @@ public class Test {
 	public static void main(String[] args) {
 		// Andrea: ho aggiunto un syso per stampare
 		System.out.println("stampa");
+		
+		
+		// Pippo: ho aggiunto una seconda stampa
+		System.out.println("stampa pippo");
 	}
 
 }
